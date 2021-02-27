724d75620a64d228d1a843d58af3327550cbbe25
Dominik <dominik-nowak@hotmail.com>
2021-02-27 21:47:54 +0100

    add database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'remotemysql.com',
+      port: 3306,
+      username: 'TtEhSSiGPc',
+      password: 'URLyXZVVER',
+      database: 'TtEhSSiGPc',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
