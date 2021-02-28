83df7fe68155e9137cd4212af7ceb0d89034b5ae
Enessetere <muszalski.sebastian@gmail.com>
2021-02-28 22:08:52 +0100

    feat: change DB exposed port

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { UsersModule } from './users/users.module';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
-      port: 3306,
+      port: 3307,
       username: 'root',
       password: 'password',
       database: 'ffts',
