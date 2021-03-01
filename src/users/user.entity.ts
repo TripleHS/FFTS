62c9651415fc13c9923d2098346ffbc09ee53754
Dominik <dominik-nowak@hotmail.com>
2021-03-01 21:38:28 +0100

    extend user.entity

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,13 +1,31 @@
+import { IsEmail } from 'class-validator';
+import { UserRole } from 'src/enums';
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
-  id: number;
+  id: BigInt;
 
-  @Column()
+  @Column({ length: 25 })
   firstName: string;
 
-  @Column()
+  @Column({ length: 50 })
   lastName: string;
+
+  @Column({ nullable: false, unique: true })
+  username: string;
+
+  @Column({ length: 255 })
+  userPassword: string;
+
+  @Column({ nullable: false, unique: true, length: 50 })
+  @IsEmail()
+  email: string;
+
+  @Column({ nullable: false, length: 15 })
+  userRole: UserRole;
+
+  @Column({ length: 1000 })
+  description: string;
 }
