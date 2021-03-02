b7d57153a7b5d665d045b13f60cbb907f57d7fed
Dominik <dominik-nowak@hotmail.com>
2021-03-02 20:11:11 +0100

    add plugin to generate swagger's decorators

---
+++ test/cli-config.ts
@@ -0,0 +1,14 @@
+import transformer from '@nestjs/swagger/plugin';
+
+module.exports.name = 'nestjs-swagger-transformer';
+// you should change the version number anytime you change the configuration below - otherwise, jest will not detect changes
+module.exports.version = 1;
+
+module.exports.factory = (cs) => {
+  return transformer.before(
+    {
+      // @nestjs/swagger/plugin options (can be empty)
+    },
+    cs.tsCompiler.program,
+  );
+};
