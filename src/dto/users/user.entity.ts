17455b7866aa28f25133d76acde70aba77898ae8
Dominik <dominik-nowak@hotmail.com>
2021-03-08 19:52:02 +0100

    fix: change @Length decorator for @MinLength and @MaxLength

--- src/dto/users/user.entity.ts
+++ src/dto/users/user.entity.ts
@@ -4,8 +4,8 @@ import {
   IsOptional,
   IsString,
   IsUUID,
-  Length,
   MaxLength,
+  MinLength,
 } from 'class-validator';
 import { UserRole } from 'src/enums';
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
@@ -19,25 +19,29 @@ export class User {
   @Column({ nullable: true, name: 'first_name', length: 25 })
   @IsOptional()
   @IsString()
-  @Length(2, 25)
+  @MinLength(2)
+  @MaxLength(25)
   firstName?: string;
 
   @Column({ nullable: true, name: 'last_name', length: 50 })
   @IsOptional()
   @IsString()
-  @Length(2, 50)
+  @MinLength(2)
+  @MaxLength(50)
   lastName?: string;
 
   @Column({ nullable: false, unique: true, length: 50 })
   @IsNotEmpty()
   @IsString()
-  @Length(2, 50)
+  @MinLength(2)
+  @MaxLength(50)
   username: string;
 
   @Column({ nullable: false, name: 'user_password', length: 255 })
   @IsNotEmpty()
   @IsString()
-  @Length(7, 255)
+  @MinLength(7)
+  @MaxLength(255)
   userPassword: string;
 
   @Column({ nullable: false, unique: true, length: 50 })
