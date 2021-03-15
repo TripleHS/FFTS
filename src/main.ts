96b0b65b0ae8e842e356c10dd1a0fc3881edf6c1
Dominik <dominik-nowak@hotmail.com>
2021-03-15 20:55:27 +0100

    heroku stuff

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
