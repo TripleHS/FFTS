9ac5c4a882c5ea63ee63e435f9454d1b75ab4850
Dominik <dominik-nowak@hotmail.com>
2021-03-15 22:09:38 +0100

    fix: change port to 5000

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const PORT = process.env.PORT || 8080;
+  const PORT = process.env.PORT || 5000;
 
   app.useGlobalPipes(new ValidationPipe());
 
