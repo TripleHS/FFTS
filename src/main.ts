b3374ecd81fde8d8bf96970ff6d7d8d90791921f
Dominik <dominik-nowak@hotmail.com>
2021-03-15 21:48:22 +0100

    fix: pass host

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(8080);
+  await app.listen(process.env.PORT || 8080, '0.0.0.0');
 }
 bootstrap();
