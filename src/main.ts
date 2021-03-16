ecd7ef9d0689137c1621f09016ea154dba541b7a
Dominik <dominik-nowak@hotmail.com>
2021-03-16 15:17:51 +0100

    fix: add hostname to listen

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+  await app.listen(PORT, '0.0.0.0', () => console.log(`Listening on ${PORT}`));
 }
 bootstrap();
