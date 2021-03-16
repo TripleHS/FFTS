7138b281018d3cf1885138c2b9336d22b46ff48a
Dominik <dominik-nowak@hotmail.com>
2021-03-16 14:58:05 +0100

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,9 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const PORT = process.env.PORT;
 
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
@@ -19,6 +21,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT);
+  await app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 }
 bootstrap();
